1cfef1999d442d55db16740c7a68fd6fc03a321d
Flavius Gavriluta <116315036+FlaviusGavriluta@users.noreply.github.com>
2024-02-27 13:14:43 +0200

    Update EasyLog.java

--- src/main/java/org/example/EasyLog.java
+++ src/main/java/org/example/EasyLog.java
@@ -1,7 +1,8 @@
 package org.example;
+import static java.lang.Math.log;
 
 public class EasyLog {
     public static double logs(double base, double A, double B) {
-        return Math.log(A * B) / Math.log(base);
+        return log(A * B) / log(base);
     }
 }
